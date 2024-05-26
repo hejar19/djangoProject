548e34c200729f2145f8cb4a5926ab17d2a42106
hejar19 <hajarelyamani03@gmail.com>
2024-05-26 18:23:00 +0100

    types

--- schoolapp/urls.py
+++ schoolapp/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('timetable/', views.timetable, name='timetable'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', views.CustomLoginView.as_view(template_name='login.html'), name='login'),
 ]
